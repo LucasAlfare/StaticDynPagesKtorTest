156ee218d01b74f89056a65cb65cc52abfe85590
LucasAlfare <god-lucas@hotmail.com>
2024-05-16 00:54:16 -0300

    changed folders to use classpath resources

--- src/main/kotlin/com/lucasalfare/Main.kt
+++ src/main/kotlin/com/lucasalfare/Main.kt
@@ -8,7 +8,6 @@ import io.ktor.server.netty.*
 import io.ktor.server.plugins.cors.routing.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import java.io.File
 
 fun main() {
   embeddedServer(Netty, port = 80) {
@@ -22,13 +21,11 @@ fun main() {
         call.respondText("This text is not from an HTML file. Go to the endpoint '/home' to see a static HTML file served by this backend.")
       }
 
-      staticFiles(
+      staticResources(
         remotePath = "/home",
-        dir = File("pages"),
-        index = "my_home_page.html"
-      ) {
-        enableAutoHeadResponse()
-      }
+        basePackage = "assets",
+        index = "pages/my_home_page.html"
+      )
     }
   }.start(true)
 }
